4115b7734f024b56cb9e941a3638a43a471fa936
dahhou-ilyas <dahhou.ilyas@gmail.com>
2022-12-23 12:50:24 +0100

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const app=express();
+
+const Port=3000;
+
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.post('/',(req,res)=>{
+    let data=req.body
+    console.log(data);
+      
+})
